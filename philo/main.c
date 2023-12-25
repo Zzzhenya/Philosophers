9ae9cf773cc758f2bd842c5275b018e0574d88f4
Shenya De Silva <shenya44@outlook.com>
2023-12-25 18:48:23 +0100

    Simple thread and thread function program

--- philo/main.c
+++ philo/main.c
@@ -1,7 +1,28 @@
 #include "libphilo.h"
 
+static void	*threadfunc(void *arg)
+{
+	char *s;
+
+	s = (char *)arg;
+	printf("%s\n", s);
+	return ((void *) strlen(s));
+}
+
 int main(void)
 {
-	printf("Hello World!\n");
+	pthread_t t1;
+	void	*res;
+	int 	s;
+
+	s = pthread_create(&t1, NULL, threadfunc, "Hello World!");
+	//int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg);
+	if (s != 0)
+		err_exit("pthread_create() error.");
+	printf("This is the main func\n");
+	s = pthread_join(t1, &res);
+	if (s != 0)
+		err_exit("pthread_join() error.");
+	printf("Thread returned %ld\n", (long)res);
 	return (0);
 }
