2e20ca82de74010e836edfa25c3adfcf813b83b9
Shenya De Silva <shenya44@outlook.com>
2023-12-25 16:56:31 +0100

    Initial commit

---
+++ philo/libphilo.h
@@ -0,0 +1,10 @@
+#ifndef LIBPHILO_H
+# define LIBPHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <unistd.h>
+
+/* main.c */
+
+#endif
---
+++ philo/main.c
@@ -0,0 +1,7 @@
+#include "libphilo.h"
+
+int main(void)
+{
+	printf("Hello World!\n");
+	return (0);
+}
