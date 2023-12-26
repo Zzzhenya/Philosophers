2ac5b3d4844be82884d2363f22101cdc124be6d9
Shenya De Silva <shenya44@outlook.com>
2023-12-26 15:43:50 +0100

    Changed to errexit

--- philo/main.c
+++ philo/main.c
@@ -13,18 +13,13 @@ int main(void)
 {
 	pthread_t t1;
 	void	*res;
-	int 	s;
 
-	s = pthread_create(&t1, NULL, threadfunc, "Hello World!");
+	if (pthread_create(&t1, NULL, threadfunc, "Hello World!") != 0)
 	//int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg);
-	if (s != 0)
-		errno_exit(s, "pthread_create() error.");
-	printf("This is the main func\n");
-	s = pthread_join(t1, &res);
-	if (s != 0)
-		errno_exit(s, "pthread_join() error.");
+		err_exit("pthread_create() error.");
+	if (pthread_join(t1, &res) != 0)
+		err_exit("pthread_join() error.");
 	printf("Thread returned %ld\n", (long)res);
 	system("leaks ./philo");
-	exit(0);
-	//return (0);
+	return (0);
 }
