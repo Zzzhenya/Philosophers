4860a7c6a4163e0f8358a7f523fc3cb7beec6d2c
Shenya De Silva <shenya44@outlook.com>
2023-12-26 17:09:15 +0100

    Program with mutex

--- philo/main.c
+++ philo/main.c
@@ -1,6 +1,7 @@
 #include "libphilo.h"
 
 long i = 0;
+pthread_mutex_t mutex;
 
 static void	*threadfunc()
 {
@@ -8,11 +9,13 @@ static void	*threadfunc()
 	
 	while (count < 10)
 	{
+		pthread_mutex_lock(&mutex);
 		printf("Message from thread: i:%ld\n", i ++);
 		count ++;
+		pthread_mutex_unlock(&mutex);
 		usleep(1000);
 	}
-	printf("End...i: %ld\n", i);
+	printf("End...\n");
 	return ((void *)i);
 }
 
@@ -21,6 +24,7 @@ int main(void)
 	pthread_t t1, t2;
 	void	*res;
 
+	pthread_mutex_init(&mutex, NULL);
 	//int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void *), void *arg);
 	if (pthread_create(&t1, NULL, threadfunc, NULL) != 0)
 		err_exit("pthread_create() error.");
@@ -28,9 +32,8 @@ int main(void)
 		err_exit("pthread_create() error.");
 	if (pthread_join(t1, &res) != 0)
 		err_exit("pthread_join() error.");
-	printf("Thread t1 returned %ld\n", *(long *)res);
 	if (pthread_join(t2, &res) != 0)
 		err_exit("pthread_join() error.");
-	printf("Thread t2 returned %ld\n", *(long *)res);
+	pthread_mutex_destroy(&mutex);
 	return (0);
 }
